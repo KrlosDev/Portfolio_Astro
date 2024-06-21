65bf5ff8b8efa91f394d48cc3ae05162097a91d2
KrlosDev <carlosb@bbhint.com>
2024-06-21 18:44:58 -0500

    fixing issues with descriptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://krlosdev.com/',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'static'
 });
