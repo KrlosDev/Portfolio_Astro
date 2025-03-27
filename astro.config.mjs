23144302c0db2c259a56c8d2aa05ffc6ad729e8c
Krlosdev <carlosb@bbhint.com>
2025-03-26 22:18:20 -0500

    making needed changes for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://krlosdev.com/',
+  site: 'https://krlosdev.github.io/',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'static'
+  base: '/Portfolio_Astro', 
 });
