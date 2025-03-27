560a3cbd250dc1b7bfb2a27955680e60decc875f
Krlosdev <carlosb@bbhint.com>
2025-03-26 22:53:46 -0500

    Moving images to src folder as recommended by astro documentation to be available to use sharp for image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://krlosdev.github.io/',
   integrations: [mdx(), sitemap(), tailwind()],
-  base: '/Portfolio_Astro', 
+  base: '', 
 });
